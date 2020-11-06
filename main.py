5bba6edd4caeadd80cdcd379309dc529eedc918e
Jasper Peshek <jpeshek24@biomedscienceacademy.org>
2020-11-06 22:34:33 +0000

    I made part of the madlibs

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Jasper Peshek
+Period/Core: 7
 
 
 """
+
+txt1 = input("Enter a name: ")
+txt2 = input("Enter a place: ")
+txt3 = input("Enter a verb: ")
+txt4 = input("Enter an adverb: ")
+txt5 = input("Enter an adjective: ")
+txt6 = input("Enter a noun: ")
+txt7 = input("Enter a pronoun: ")
+txt8 = input("Enter a different name: ")
+txt9 = input("Enter an adjective: ")
+txt10 = input("Enter an adjective: ")
+
+print('{} '.format(txt1) + "has always wanted to go to " + txt2 + ".")
+print(txt7 + " wants to go there to " + txt3 + " " + txt4 + ".")
+print(txt1 + " is a " + txt5 +" "+ txt6)
+print(txt7 + " does what" +" "+ txt7 +" "+ "likes.")
